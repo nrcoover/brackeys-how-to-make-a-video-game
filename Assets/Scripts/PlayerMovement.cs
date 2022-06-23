8d85538fe59932135d4bd2cd521ed3a3a925c0b7
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-06-23 12:59:43 -0400

    Adds Functionality to PlayerMovement And Adds Physics to Ground and Player
    The player can now move their avatar left and right with the "a" and "d" keys; also, a forward force has been implemented to automatically cause the avatar to slide down the lane.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,8 @@
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rigidBody;
+    public float forwardForce = 2000f;
+    public float sidewasyForce = 500f;
 
     // Start is called before the first frame update
     void Start()
@@ -11,8 +13,20 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    // Marked as "Fixed"Update because of the manipulation of physics
+    void FixedUpdate()
     {
+        // Adds a forward force
+        rigidBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+    
+        if (Input.GetKey("d"))
+        {
+            rigidBody.AddForce(sidewasyForce * Time.deltaTime, 0, 0);
+        }
 
+        if (Input.GetKey("a"))
+        {
+            rigidBody.AddForce(-sidewasyForce * Time.deltaTime, 0, 0);
+        }
     }
 }
