9abd12d515bd1fe2f22969bb5a2acb89daa916f1
nrcoover <81087345+nrcoover@users.noreply.github.com>
2022-06-23 12:20:09 -0400

    initial commit
    Started development on a new game, following Brackeys' Game Tutorial Series on youtube, titled "How To Make A Video Game".

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
